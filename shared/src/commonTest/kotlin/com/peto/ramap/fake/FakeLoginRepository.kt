cacf7295c88278fe057c3d46e8b55f62f5f8fefb
chanho0908 <uni070@naver.com>
2026-07-03 21:14:29 +0900

    test: 테스트코드 추가

---
+++ shared/src/commonTest/kotlin/com/peto/ramap/fake/FakeLoginRepository.kt
@@ -0,0 +1,35 @@
+package com.peto.ramap.fake
+
+import com.peto.ramap.domain.repository.LoginRepository
+import io.github.jan.supabase.auth.status.SessionStatus
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class FakeLoginRepository(
+    initialSessionStatus: SessionStatus = SessionStatus.NotAuthenticated(),
+    private val userEmail: String? = null,
+) : LoginRepository {
+    private val mutableSessionStatus = MutableStateFlow(initialSessionStatus)
+
+    var signInWithKakaoCallCount = 0
+        private set
+    var signOutCallCount = 0
+        private set
+
+    override val sessionStatus: StateFlow<SessionStatus> = mutableSessionStatus
+
+    override suspend fun awaitInitialization() = Unit
+
+    override fun hasSession(): Boolean = sessionStatus.value is SessionStatus.Authenticated
+
+    override fun currentUserEmail(): String? = userEmail
+
+    override suspend fun signInWithKakao() {
+        signInWithKakaoCallCount += 1
+    }
+
+    override suspend fun signOut() {
+        signOutCallCount += 1
+        mutableSessionStatus.value = SessionStatus.NotAuthenticated(isSignOut = true)
+    }
+}
