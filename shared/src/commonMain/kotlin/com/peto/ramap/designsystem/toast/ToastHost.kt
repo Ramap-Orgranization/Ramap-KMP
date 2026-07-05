d26100c84b357a1fa4fc2eee4ed06b6d5f0738e9
chanho0908 <uni070@naver.com>
2026-07-05 19:28:17 +0900

    design: 토스트 액션버튼 테투리 제거

--- shared/src/commonMain/kotlin/com/peto/ramap/designsystem/toast/ToastHost.kt
+++ shared/src/commonMain/kotlin/com/peto/ramap/designsystem/toast/ToastHost.kt
@@ -191,7 +191,6 @@ private fun ActionButton(
         modifier =
             Modifier
                 .background(GrayColor.C300, RoundedCornerShape(8.dp))
-                .border(1.dp, GrayColor.C500, RoundedCornerShape(8.dp))
                 .padding(vertical = 5.5.dp, horizontal = 12.dp)
                 .noRippleClickable(
                     onClick = {
