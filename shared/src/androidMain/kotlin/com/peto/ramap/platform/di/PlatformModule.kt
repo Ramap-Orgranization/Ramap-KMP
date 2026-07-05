612b40d019471a3c17f59b59c43f821ce7fc0a18
chanho0908 <uni070@naver.com>
2026-07-05 19:32:44 +0900

    chore: KtliontFormat

--- shared/src/androidMain/kotlin/com/peto/ramap/platform/di/PlatformModule.kt
+++ shared/src/androidMain/kotlin/com/peto/ramap/platform/di/PlatformModule.kt
@@ -1,7 +1,7 @@
 package com.peto.ramap.platform.di
 
-import com.peto.ramap.platform.AppSettingsOpener
 import com.peto.ramap.platform.AndroidAppSettingsOpener
+import com.peto.ramap.platform.AppSettingsOpener
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
--- shared/src/commonMain/kotlin/com/peto/ramap/di/KoinInitializer.kt
+++ shared/src/commonMain/kotlin/com/peto/ramap/di/KoinInitializer.kt
@@ -2,8 +2,8 @@ package com.peto.ramap.di
 
 import com.peto.ramap.data.datasource.di.dataSourceModule
 import com.peto.ramap.data.repository.di.repositoryModule
-import com.peto.ramap.platform.di.platformModule
 import com.peto.ramap.designsystem.di.designSystemModule
+import com.peto.ramap.platform.di.platformModule
 import com.peto.ramap.ui.login.di.loginModule
 import com.peto.ramap.ui.map.di.mapModule
 import org.koin.core.context.startKoin
