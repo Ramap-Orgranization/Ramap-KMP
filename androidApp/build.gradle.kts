c485f08bb118401722686e2f62c9a54b9fe0aeef
chanho0908 <uni070@naver.com>
2026-07-12 07:03:16 +0900

    chore: 디버그 앱 식별 설정 추가

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -94,6 +94,10 @@ android {
         }
     }
     buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
