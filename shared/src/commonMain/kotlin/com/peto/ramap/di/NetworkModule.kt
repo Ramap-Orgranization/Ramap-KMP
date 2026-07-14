31da74e027ae92f9240acb9d33ea0e567570bffd
chanho0908 <uni070@naver.com>
2026-07-14 19:53:13 +0900

    chore: Ktor 네트워크 로깅 설정 추가

--- shared/src/commonMain/kotlin/com/peto/ramap/di/NetworkModule.kt
+++ shared/src/commonMain/kotlin/com/peto/ramap/di/NetworkModule.kt
@@ -3,11 +3,19 @@ package com.peto.ramap.di
 import com.peto.ramap.network.NaverReverseGeocoder
 import com.peto.ramap.network.supabaseClient
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
 import org.koin.dsl.module
 
 val networkModule =
     module {
         single { supabaseClient }
-        single { HttpClient() }
+        single {
+            HttpClient {
+                install(Logging) {
+                    level = LogLevel.INFO
+                }
+            }
+        }
         single { NaverReverseGeocoder(get()) }
     }
