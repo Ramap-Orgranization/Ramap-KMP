700ef615e5e084466caaeaf633fab26e579e3a8b
chanho0908 <uni070@naver.com>
2026-07-15 15:48:18 +0900

    fix: 재로그인 시 Android FID 재등록

--- androidApp/src/main/kotlin/com/peto/ramap/MainApplication.kt
+++ androidApp/src/main/kotlin/com/peto/ramap/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.peto.ramap
 
 import android.app.Application
+import com.google.firebase.messaging.FirebaseMessaging
 import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
 import com.peto.ramap.core.config.RamapAppConfig
@@ -10,6 +11,7 @@ import org.koin.android.ext.koin.androidContext
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        FirebaseMessaging.getInstance().register()
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NcpKeyClient(RamapAppConfig.naverMapNcpKeyId)
         KakaoSdk.init(this, RamapAppConfig.kakaoNativeAppKey)
