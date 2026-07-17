f0640573dacde287310d4adbd3958b254d06eccf
chanho0908 <uni070@naver.com>
2026-07-17 10:11:43 +0900

    refactor: iOS 앱 모듈 연동 정리

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -32,8 +32,8 @@ struct iOSApp: App {
 
     init() {
         UnhandledExceptionLoggerKt.installUnhandledExceptionLogger()
-        NMFAuthManager.shared().ncpKeyId = RamapAppConfig.shared.naverMapNcpKeyId
-        KakaoSDK.initSDK(appKey: RamapAppConfig.shared.kakaoNativeAppKey)
+        NMFAuthManager.shared().ncpKeyId = RamapSecrets.shared.naverMapNcpKeyId
+        KakaoSDK.initSDK(appKey: RamapSecrets.shared.kakaoNativeAppKey)
         KoinInitializerKt.doInitKoin(appDeclaration: { _ in })
         NotificationCenter.default.addObserver(
             forName: Notification.Name("NotificationPermissionGranted"),
@@ -67,6 +67,10 @@ struct iOSApp: App {
         WindowGroup {
             ContentView()
                 .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                        return
+                    }
                     AuthDeepLinkHandlerKt.handleAuthDeepLink(url: url)
                 }
         }
