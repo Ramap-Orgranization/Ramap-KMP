a4d66067238ec0768f78b4a2146323b06693758a
chanho0908 <uni070@naver.com>
2026-07-17 10:11:09 +0900

    refactor: 메인 기능 모듈 구성

---
+++ feature/main/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("ramap.kmp.library")
+}
+
+kotlin {
+    sourceSets.commonMain.dependencies {
+        api(projects.feature.main.events)
+        api(projects.feature.main.map)
+        api(projects.feature.main.my)
+    }
+}
