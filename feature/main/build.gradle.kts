f0eed6f8d65f17fc0debbd4d415757f02f220cbb
chanho0908 <uni070@naver.com>
2026-07-22 19:31:52 +0900

    chore: 랭킹 기능 모듈 구성

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -6,6 +6,7 @@ kotlin {
     sourceSets.commonMain.dependencies {
         api(projects.feature.main.events)
         api(projects.feature.main.map)
+        api(projects.feature.main.ranking)
         api(projects.feature.main.my)
     }
 }
