594d543fe9a1f7a64783c54f9e9f3475f5018dae
chanho0908 <uni070@naver.com>
2026-07-28 08:42:55 +0900

    feat: iOS 애플 지도 연동

--- core/platform/src/commonMain/kotlin/com/peto/ramap/platform/ExternalUriOpener.kt
+++ core/platform/src/commonMain/kotlin/com/peto/ramap/platform/ExternalUriOpener.kt
@@ -4,4 +4,13 @@ expect object ExternalUriOpener {
     fun open(uri: String)
 
     fun isSupportedWebUri(uri: String): Boolean
+
+    val isAppleMapsAvailable: Boolean
+
+    fun openAppleMaps(
+        name: String,
+        address: String,
+        latitude: Double,
+        longitude: Double,
+    )
 }
