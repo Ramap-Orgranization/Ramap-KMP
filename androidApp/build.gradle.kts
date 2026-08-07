0597faf07cb8549462eb346321c1c8411228656a
페토 <84930748+chanho0908@users.noreply.github.com>
2026-08-07 22:04:26 +0900

    Release -> develop (#173)
    * chore: Android 앱 버전 1.0.2로 업데이트
    
    * chore: Android 앱 버전 1.0.3으로 업데이트
    
    * chore: Android 앱 버전 1.0.4으로 업데이트
    
    * chore: Android 앱 버전 1.0.5로 업데이트
    
    * fix: 광고 ID 수집 비활성화
    
    * chore: Android 앱 버전 1.0.6으로 업데이트
    
    * chore: Android 앱 버전 1.0.7으로 업데이트
    
    * chore: Android 앱 버전 1.0.8으로 업데이트
    
    * chore: Android 앱 버전 1.0.9으로 업데이트
    
    * fix: iOS Xcode 프로젝트 파일 복구
    
    * chore: Android 앱 버전 1.0.10으로 업데이트

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -101,8 +101,8 @@ android {
             libs.versions.android.target.sdk
                 .get()
                 .toInt()
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 11
+        versionName = "1.0.10"
         manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoNativeAppKey
     }
     packaging {
