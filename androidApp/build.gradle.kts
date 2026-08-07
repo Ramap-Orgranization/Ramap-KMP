c6f419032f0c7101a6a5361cc005507e6da03896
chanho0908 <uni070@naver.com>
2026-08-07 20:18:45 +0900

    chore: Android 앱 버전 1.0.9으로 업데이트

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -101,8 +101,8 @@ android {
             libs.versions.android.target.sdk
                 .get()
                 .toInt()
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
         manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoNativeAppKey
     }
     packaging {
