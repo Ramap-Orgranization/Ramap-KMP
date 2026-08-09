fd3bd3f0c6cb78040dbb6886078565ab6a2a9c92
chanho0908 <uni070@naver.com>
2026-08-09 19:21:44 +0900

    style: Android 플랫폼 모듈 import 정렬

--- shared/src/androidMain/kotlin/com/peto/ramap/di/PlatformModule.kt
+++ shared/src/androidMain/kotlin/com/peto/ramap/di/PlatformModule.kt
@@ -6,10 +6,10 @@ import com.peto.ramap.analytics.FirebaseAnalyticsTracker
 import com.peto.ramap.analytics.FirebaseCrashReporter
 import com.peto.ramap.platform.AndroidAppSettingsOpener
 import com.peto.ramap.platform.AndroidAppVersionProvider
-import com.peto.ramap.platform.MapSearchHistoryStorage
-import com.peto.ramap.platform.createMapSearchHistoryStorage
 import com.peto.ramap.platform.AppSettingsOpener
 import com.peto.ramap.platform.AppVersionProvider
+import com.peto.ramap.platform.MapSearchHistoryStorage
+import com.peto.ramap.platform.createMapSearchHistoryStorage
 import com.peto.ramap.platform.location.AndroidCurrentLocationProvider
 import com.peto.ramap.platform.location.CurrentLocationProvider
 import org.koin.android.ext.koin.androidContext
