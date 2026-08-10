d78492a406799fb7e2be6831b039e40144acd7da
chanho0908 <uni070@naver.com>
2026-08-10 20:16:40 +0900

    fix: 지역 리소스 매퍼 import 정렬

--- core/ui/src/commonMain/kotlin/com/peto/ramap/ui/resource/area/AreaFilterResourceMapper.kt
+++ core/ui/src/commonMain/kotlin/com/peto/ramap/ui/resource/area/AreaFilterResourceMapper.kt
@@ -1,7 +1,7 @@
 package com.peto.ramap.ui.resource.area
 
-import com.peto.ramap.domain.model.shop.AreaFilter
 import com.peto.ramap.designsystem.resource.UiText
+import com.peto.ramap.domain.model.shop.AreaFilter
 import ramap.shared.generated.resources.Res
 import ramap.shared.generated.resources.ranking_all_regions
 import ramap.shared.generated.resources.ranking_district_label
