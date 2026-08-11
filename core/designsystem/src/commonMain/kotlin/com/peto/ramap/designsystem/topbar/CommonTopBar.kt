5dff8029b0863fb7880e85865ad93c0e62b9c008
chanho0908 <uni070@naver.com>
2026-08-11 16:36:43 +0900

    design: 공통 상단바 높이 조정

--- core/designsystem/src/commonMain/kotlin/com/peto/ramap/designsystem/topbar/CommonTopBar.kt
+++ core/designsystem/src/commonMain/kotlin/com/peto/ramap/designsystem/topbar/CommonTopBar.kt
@@ -28,7 +28,7 @@ fun CommonTopBar(
             modifier =
                 Modifier
                     .fillMaxWidth()
-                    .height(40.dp),
+                    .height(48.dp),
         ) {
             Box(
                 modifier =
