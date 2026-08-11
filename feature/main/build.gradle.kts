b23af40ced1b1af2567d60062ad0af720b40b574
chanho0908 <uni070@naver.com>
2026-08-11 16:36:16 +0900

    chore: 캘린더 모듈 및 의존성 등록

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -6,6 +6,7 @@ kotlin {
     sourceSets.commonMain.dependencies {
         api(projects.feature.main.event.list)
         api(projects.feature.main.event.detail)
+        api(projects.feature.main.event.calendar)
         api(projects.feature.main.map)
         api(projects.feature.main.ranking)
         api(projects.feature.main.my)
