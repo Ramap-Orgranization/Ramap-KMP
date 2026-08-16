c3412e852de7af550062b1f0306c19cdb6693d3e
chanho0908 <uni070@naver.com>
2026-08-16 23:23:29 +0900

    chore: Android 앱 버전 1.1.3으로 업데이트

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -101,8 +101,8 @@ android {
             libs.versions.android.target.sdk
                 .get()
                 .toInt()
-        versionCode = 17
-        versionName = "1.1.2"
+        versionCode = 18
+        versionName = "1.1.3"
         manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoNativeAppKey
     }
     packaging {
