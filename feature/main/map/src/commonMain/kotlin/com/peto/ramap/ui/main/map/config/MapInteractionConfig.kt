0a97575f52da4cd24443899bf6394baff546d452
chanho0908 <uni070@naver.com>
2026-08-25 19:19:08 +0900

    design: 지도 최대 축소 레벨 변경 (9 -> 6)

--- feature/main/map/src/commonMain/kotlin/com/peto/ramap/ui/main/map/config/MapInteractionConfig.kt
+++ feature/main/map/src/commonMain/kotlin/com/peto/ramap/ui/main/map/config/MapInteractionConfig.kt
@@ -1,7 +1,7 @@
 package com.peto.ramap.ui.main.map.config
 
 internal object MapInteractionConfig {
-    const val MAX_ZOOM_OUT_LEVEL = 9
+    const val MAX_ZOOM_OUT_LEVEL = 6
     const val CLUSTER_MAX_ZOOM_LEVEL = 12
     const val SELECTED_MARKER_ZOOM_LEVEL = 17
     const val SELECTED_MARKER_BOTTOM_PADDING_RATIO = 0.35f
